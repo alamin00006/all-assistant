3562565329d0a24b6bf98684c3c083ab6b3ceaa3
Muhibbul hasan <muhibbulhasan6@gmail.com>
2023-02-28 18:15:25 +0600

    header navbar

--- src/views/Header/Header.jsx
+++ src/views/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="navbar bg-slate-400 text-black">
+    <div className="navbar bg-slate-300 text-black">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -60,32 +60,14 @@ const Header = () => {
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Item 1</a>
+            <a>Home</a>
           </li>
-          <li tabIndex={0}>
-            <a>
-              Parent
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="p-2">
-              <li>
-                <a>Submenu 1</a>
-              </li>
-              <li>
-                <a>Submenu 2</a>
-              </li>
-            </ul>
+
+          <li>
+            <a>Cart</a>
           </li>
           <li>
-            <a>Item 3</a>
+            <a className="text-gray-500">Login</a>
           </li>
         </ul>
       </div>
