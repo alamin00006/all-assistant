7d1ec91ad9cf15239ae6ed8c94e8550fc9ea797c
Muhibbul hasan <muhibbulhasan6@gmail.com>
2023-02-28 13:15:14 +0600

    banner

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,14 @@
 import React from "react";
+import Banner from "../../views/Banner/Banner";
 import Header from "../../views/Header/Header";
 
 const Home = () => {
-<<<<<<< HEAD
   return (
     <div>
       <Header></Header>
+      <Banner></Banner>
     </div>
   );
-=======
-  return <div>hellooooooooo</div>;
->>>>>>> 0dfe041f02d89147d2e7f13456b92d6326a4ff69
 };
 
 export default Home;
--- src/views/Banner/Banner.jsx
+++ src/views/Banner/Banner.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import img1 from "../../assets/images/banner1.jpg";
+import img2 from "../../assets/images/banner2.jpg";
 
+// Import Swiper styles
+import "swiper/css";
+// import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./banner.css";
+
+// import required modules
+import { Autoplay, Navigation } from "swiper";
 const Banner = () => {
-  return <div></div>;
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        modules={[Autoplay, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={img1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img2} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
 };
 
 export default Banner;
