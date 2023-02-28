c75772443d8f3d759788240194a568d5fad4294f
fahimnuman <fahiomnuman87@gmail.com>
2023-02-28 16:15:10 +0600

    UserInfo Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Navbar from "./components/Header/Navbar/Navbar";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="max-w-[1440px] mx-auto">
-      <Navbar />
+      <Home></Home>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
+import UserInfo from "../../views/UserInfo/UserInfo";
 
 const Home = () => {
-<<<<<<< HEAD
-  return <div>gfhfgh</div>;
-=======
-  return <div>hellooooooooo</div>;
->>>>>>> 0dfe041f02d89147d2e7f13456b92d6326a4ff69
+
+  return(
+    <div>
+      <UserInfo></UserInfo>
+    </div>
+  );
+
+  
 };
 
 export default Home;
