b99de5254c88b3494e1ce7fa97457a847a87d141
fahimnuman <fahiomnuman87@gmail.com>
2023-03-01 12:37:15 +0600

    property category added

--- src/views/House/House.js
+++ src/views/House/House.js
@@ -1,9 +1,11 @@
 import React from "react";
+import PropertyCategory from "../PropertyCategory/PropertyCategory";
 
 const House = () => {
   return (
     <div>
       <h2>House Page</h2>
+      <PropertyCategory></PropertyCategory>
     </div>
   );
 };
