54d0146d4ba8d9b5d5dd90e65d28d41e905e1e48
Muhibbul hasan <muhibbulhasan6@gmail.com>
2023-03-01 15:32:38 +0600

    districts

--- src/views/House/House.js
+++ src/views/House/House.js
@@ -4,22 +4,30 @@ import SearchOption from "../SearchOption/SearchOption";
 
 const House = () => {
   const [divisions, setDivision] = useState([]);
-  // const [districts, setDistricts] = useState([]);
+  const [districts, setDistricts] = useState([]);
   useEffect(() => {
     fetch("bd-division.json")
       .then((res) => res.json())
       .then((data) => setDivision(data.divisions));
   }, []);
 
-  // useEffect(() => {
-  //   fetch("bd-division.json")
-  //     .then((res) => res.json())
-  //     .then((data) => setDistricts(data.districts));
-  // }, []);
-  // console.log(divisions);
+  useEffect(() => {
+    fetch("bd-districts.json")
+      .then((res) => res.json())
+      .then((data) => setDistricts(data.districts));
+  }, []);
+  // console.log(districts);
+
+  const handleDistricts = (e) => {
+    console.log(e);
+  };
+
   return (
     <div>
-      <SearchOption divisions={divisions}></SearchOption>
+      <SearchOption
+        divisions={divisions}
+        handleDistricts={handleDistricts}
+      ></SearchOption>
       <RecentProperties></RecentProperties>
     </div>
   );
--- src/views/SearchOption/SearchOption.jsx
+++ src/views/SearchOption/SearchOption.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 
-const SearchOption = ({ divisions }) => {
-  //   console.log(divisions);
+const SearchOption = ({ divisions, handleDistricts }) => {
+  //   console.log(handleDistricts);
   return (
     <div>
       Search Option
       <div>
-        <select className="select w-full max-w-xs">
-          <option defaultValue="--Select--">--Select--</option>
+        <select
+          name="district"
+          className="select w-full max-w-xs"
+          onChange={(e) => handleDistricts(e.target.value)}
+        >
+          <option disabled defaultValue="--Select--">
+            --Select--
+          </option>
           {divisions.map((div) => (
-            <option key={div.id}>{div.name}</option>
+            <option value={div.id} key={div.id}>
+              {div.id} {div.name}
+            </option>
           ))}
         </select>
       </div>
