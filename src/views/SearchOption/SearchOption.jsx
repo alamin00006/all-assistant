d159666936093828b3266af2b4c7ea4f6d090030
Muhibbul hasan <muhibbulhasan6@gmail.com>
2023-03-01 16:17:54 +0600

    search option

--- src/views/SearchOption/SearchOption.jsx
+++ src/views/SearchOption/SearchOption.jsx
@@ -16,7 +16,7 @@ const SearchOption = ({ divisions, handleDistricts, getDistricts }) => {
           </option>
           {divisions.map((div) => (
             <option value={div.id} key={div.id}>
-              {div.id} {div.name}
+              {div.name}
             </option>
           ))}
         </select>
@@ -29,6 +29,30 @@ const SearchOption = ({ divisions, handleDistricts, getDistricts }) => {
               {dis.name}
             </option>
           ))}
+        </select>
+
+        <select name="price" className="select w-full max-w-xs">
+          <option disabled defaultValue="--Select--">
+            --Select--
+          </option>
+          <option>Up to 1000</option>
+          <option>Up to 2000</option>
+          <option>Up to 4000</option>
+          <option>Up to 6000</option>
+          <option>Up to 8000</option>
+          <option>Up to 10000</option>
+        </select>
+
+        <select name="price" className="select w-full max-w-xs">
+          <option disabled defaultValue="--Select--">
+            --Select--
+          </option>
+          <option>One</option>
+          <option>Two</option>
+          <option>Three</option>
+          <option>Four</option>
+          <option>Five</option>
+          <option>Six</option>
         </select>
       </div>
     </div>
