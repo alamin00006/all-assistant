00979bd7cdf014ab9d82f1decabcb21a188e5856
Muhibbul hasan <muhibbulhasan6@gmail.com>
2023-03-02 17:04:12 +0600

    overview

--- src/views/HouseDetails/HouseDetails.jsx
+++ src/views/HouseDetails/HouseDetails.jsx
@@ -25,61 +25,145 @@ const HouseDetails = () => {
             <p>Location : North Babukhan, Babukhan, Sadar, Rangpur</p>
             <button className="btn bg-lime-500 text-white">For Family</button>
           </div>
-          <div className="flex flex-row justify-center text-left">
-            <div className="mr-4">
-              <div>
-                <h2>2 Bedroom</h2>
-                <p>No. of Bedroom</p>
+          <div>
+            <div className="flex flex-row justify-center text-left">
+              <div className="mr-4">
+                <div>
+                  <h2>2 Bedroom</h2>
+                  <p>No. of Bedroom</p>
+                </div>
+                <div>
+                  <h2>Yes</h2>
+                  <p>Dining Space</p>
+                </div>
+                <div>
+                  <h2>1</h2>
+                  <p>Dining Space</p>
+                </div>
+                <div>
+                  <h2>2</h2>
+                  <p>Balcony</p>
+                </div>
+                <div>
+                  <h2>Renovated</h2>
+                  <p>Property condition</p>
+                </div>
+                <div>
+                  <h2>6500 (Fixed)</h2>
+                  <p>Rent</p>
+                </div>
               </div>
               <div>
-                <h2>Yes</h2>
-                <p>Dining Space</p>
-              </div>
-              <div>
-                <h2>1</h2>
-                <p>Dining Space</p>
-              </div>
-              <div>
-                <h2>2</h2>
-                <p>Balcony</p>
-              </div>
-              <div>
-                <h2>Renovated</h2>
-                <p>Property condition</p>
-              </div>
-              <div>
-                <h2>6500 (Fixed)</h2>
-                <p>Rent</p>
+                <div>
+                  <h2>2 Bedroom</h2>
+                  <p>No. of Bedroom</p>
+                </div>
+                <div>
+                  <h2>Yes</h2>
+                  <p>Dining Space</p>
+                </div>
+                <div>
+                  <h2>1</h2>
+                  <p>Dining Space</p>
+                </div>
+                <div>
+                  <h2>2</h2>
+                  <p>Balcony</p>
+                </div>
+                <div>
+                  <h2>Renovated</h2>
+                  <p>Property condition</p>
+                </div>
+                <div>
+                  <h2>6500 (Fixed)</h2>
+                  <p>Rent</p>
+                </div>
               </div>
             </div>
-            <div>
-              <div>
-                <h2>2 Bedroom</h2>
-                <p>No. of Bedroom</p>
-              </div>
-              <div>
-                <h2>Yes</h2>
-                <p>Dining Space</p>
-              </div>
-              <div>
-                <h2>1</h2>
-                <p>Dining Space</p>
-              </div>
-              <div>
-                <h2>2</h2>
-                <p>Balcony</p>
-              </div>
-              <div>
-                <h2>Renovated</h2>
-                <p>Property condition</p>
-              </div>
-              <div>
-                <h2>6500 (Fixed)</h2>
-                <p>Rent</p>
-              </div>
+            <div className="my-4">
+              <button className="btn bg-lime-500 text-white mr-4">
+                Get Phone Number
+              </button>
+              <button className="btn btn-outline">Share</button>
             </div>
           </div>
         </div>
+      </div>
+
+      {/* <div className="flex flex-row">
+        <div className="flex flex-row mr-6">
+          <div>
+            <h1>Kitchen</h1>
+            <h1>Furnishing</h1>
+            <h1>Gas Supply</h1>
+            <h1>CCTV Camera</h1>
+            <h1>Store Room</h1>
+            <h1>Lift</h1>
+          </div>
+          <div>
+            <h1>Yes</h1>
+            <h1>No</h1>
+            <h1>Cylinder</h1>
+            <h1>No</h1>
+            <h1>No</h1>
+            <h1>No</h1>
+          </div>
+        </div>
+        <div className="flex flex-row">
+          <div>
+            <h1>Kitchen</h1>
+            <h1>Furnishing</h1>
+            <h1>Gas Supply</h1>
+            <h1>CCTV Camera</h1>
+            <h1>Store Room</h1>
+            <h1>Lift</h1>
+          </div>
+          <div>
+            <h1>Yes</h1>
+            <h1>No</h1>
+            <h1>Cylinder</h1>
+            <h1>No</h1>
+            <h1>No</h1>
+            <h1>No</h1>
+          </div>
+        </div>
+      </div> */}
+
+      <div className="overflow-x-auto mt-7">
+        <table className="table w-full border border-sky-500">
+          {/* head */}
+          {/* <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead> */}
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+              <td>Blue</td>
+            </tr>
+            {/* row 2 */}
+            <tr>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+              <td>Purple</td>
+              <td>Purple</td>
+            </tr>
+            {/* row 3 */}
+            <tr>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>Red</td>
+              <td>Red</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
