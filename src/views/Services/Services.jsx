37cd2d2aebd8855d759dccda12cff9a195123127
Muhibbul hasan <muhibbulhasan6@gmail.com>
2023-03-08 23:04:51 +0600

    service

--- src/views/Services/Services.jsx
+++ src/views/Services/Services.jsx
@@ -2,9 +2,12 @@ import house from "../../assets/images/1.png";
 import hotel from "../../assets/images/7.png";
 import shifting from "../../assets/images/2.png";
 import technician from "../../assets/images/3.png";
-import pureMil from "../../assets/images/milk.png";
-import villagePur from "../../assets/images/Village.png";
-import gas from "../../assets/images/6 (1).png";
+import fish from "../../assets/images/fish (1).png";
+import carRent from "../../assets/images/car rent.png";
+import furnutere from "../../assets/images/furniture.png";
+import grocery from "../../assets/images/grocery.png";
+import electronic from "../../assets/images/electronic.png";
+import meat from "../../assets/images/meat (1).png";
 import { Link } from "react-router-dom";
 const Services = () => {
   return (
@@ -15,11 +18,11 @@ const Services = () => {
         </h2>
       </div>
 
-      <div className="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 mt-5 justify-items-center">
+      <div className="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 mt-5 justify-items-center gap-6">
         <div>
           <Link to="/house">
             <div className="flex items-center flex-col justify-center">
-              <img style={{ width: "250px" }} src={house} alt="" />
+              <img style={{ width: "200px" }} src={house} alt="" />
               <p className="text-2xl">House</p>
             </div>
           </Link>
@@ -27,30 +30,42 @@ const Services = () => {
         <div>
           <Link to="/hotel">
             <div className="flex items-center flex-col justify-center">
-              <img style={{ width: "250px" }} src={hotel} alt="" />
+              <img style={{ width: "200px" }} src={hotel} alt="" />
               <p className="text-2xl">Hotel</p>
             </div>
           </Link>
         </div>
         <div className="flex items-center flex-col justify-center">
-          <img style={{ width: "250px" }} src={shifting} alt="" />
+          <img style={{ width: "200px" }} src={shifting} alt="" />
           <p className="text-2xl">Shifting</p>
         </div>
         <div className="flex items-center flex-col justify-center">
-          <img style={{ width: "250px" }} src={technician} alt="" />
+          <img style={{ width: "200px" }} src={technician} alt="" />
           <p className="text-2xl">Technician</p>
         </div>
-        {/* <div className="lg:mt-0 xs:mt-5">
-          <img style={{ width: "100px" }} src={pureMil} alt="" />
-          <p className="ml-7">Pure Milk</p>
-        </div>
-        <div className="lg:mt-0 xs:mt-5">
-          <img style={{ width: "100px" }} src={villagePur} alt="" />
-          <p className="ml-7">Village Pur</p>
-        </div> */}
         <div className="flex items-center flex-col justify-center">
-          <img style={{ width: "250px" }} src={gas} alt="" />
-          <p className="text-2xl">Gas</p>
+          <img style={{ width: "120px" }} src={fish} alt="" />
+          <p className="text-2xl">Fish</p>
+        </div>
+        <div className="flex items-center flex-col justify-center">
+          <img style={{ width: "170px" }} src={furnutere} alt="" />
+          <p className="text-2xl">Furniture</p>
+        </div>
+        <div className="flex items-center flex-col justify-center">
+          <img style={{ width: "170px" }} src={electronic} alt="" />
+          <p className="text-2xl">Electronic</p>
+        </div>
+        <div className="flex items-center flex-col justify-center">
+          <img style={{ width: "200px" }} src={carRent} alt="" />
+          <p className="text-2xl">Car Rent</p>
+        </div>
+        <div className="flex items-center flex-col justify-center">
+          <img style={{ width: "122px" }} src={grocery} alt="" />
+          <p className="text-2xl">Grocery</p>
+        </div>
+        <div className="flex items-center flex-col justify-center">
+          <img style={{ width: "112px" }} src={meat} alt="" />
+          <p className="text-2xl">Meat</p>
         </div>
       </div>
     </div>
