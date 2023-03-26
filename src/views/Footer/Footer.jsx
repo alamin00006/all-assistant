7a22f590501fb837eab0d2586288abdccdb08eb5
Muhibbul hasan <muhibbulhasan6@gmail.com>
2023-03-26 16:21:21 +0600

    footer

--- src/views/Footer/Footer.jsx
+++ src/views/Footer/Footer.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import { AiOutlineMail } from "react-icons/ai";
 import { FaMobileAlt, FaRegAddressCard } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import logo from "../../assets/images/logo.png";
+import logo from "../../assets/images/logo1.png";
 
 const Footer = () => {
   return (
     <div className="bg-black text-white ">
       <footer className="footer p-10 ">
-        <div className="w-1/2">
+        <div className="w-1/2 md:ml-32 lg:ml:32">
           <Link>
             <img className="w-28" src={logo} alt="" />
           </Link>
 
           <p>
-            All Assistant Limited is a leading home service company in
+            Palkitopsolution Limited is a leading home service company in
             Bangladesh which is working for democratizing digital lifestyle
             across the country
           </p>
@@ -37,16 +37,16 @@ const Footer = () => {
 
           <div className="flex">
             <AiOutlineMail></AiOutlineMail>
-            <p className="ml-2">allassistant7@gmail.com</p>
+            <p className="ml-2">palkitopsolution23@gmail.com</p>
           </div>
 
           <div className="flex">
             <FaMobileAlt></FaMobileAlt>
 
-            <p className="ml-2">01646516565</p>
+            <p className="ml-2">01406799844</p>
           </div>
         </div>
-        <div>
+        <div className="mr-20">
           <span className="footer-title">Legal</span>
           <Link className="link link-hover">Terms of use</Link>
           <Link className="link link-hover">Privacy policy</Link>
@@ -54,7 +54,7 @@ const Footer = () => {
         </div>
       </footer>
       <div className=" flex justify-center">
-        <p>© 2023, All Rights Reserved By All Assistant Ltd.</p>
+        <p>© 2023, All Rights Reserved By Namir Creative BD Ltd.</p>
       </div>
     </div>
   );
